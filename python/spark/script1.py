908ebba7cba945753ad6daa0fddf70a1a44a7513
Dima Statz <dima.statz@gmail.com>
2023-03-30 21:50:00 +0300

    add pyspark basic example

--- python/spark/script1.py
+++ python/spark/script1.py
@@ -1,1 +1,17 @@
-print('hello spark')
+from pyspark.sql import SparkSession
+
+
+def main():
+    spark = SparkSession.builder\
+        .appName("Pyspark Template").getOrCreate()
+    
+    data = [('James','Smith','1991-04-01',3000),
+            ('Michael','Rose','','2000-05-19',4000)]
+    
+    columns = ["firstname","lastname","dob","salary"]
+    df = spark.createDataFrame(data=data, schema = columns)
+    df.show()
+
+
+if __name__ == "__main__":
+    main()
